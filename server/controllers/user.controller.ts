3dee46e880a74139954734ab9be5367044592119
suraj dhankad <94227272+Suraj-Dhankad2025@users.noreply.github.com>
2023-10-23 14:28:12 +0530

    added authentication and authorisation

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -7,6 +7,8 @@ import jwt, { Secret } from "jsonwebtoken";
 import ejs from "ejs";
 import path from "path";
 import sendMail from "../utils/sendMail";
+import { sendToken } from "../utils/jwt";
+import { redis } from "../utils/redis";
 
 //register user
 interface IRegisterationBody {
@@ -76,3 +78,78 @@ export const createActivationToken = (user: any): IActivationToken => {
   );
   return { token, activationCode };
 };
+
+//activate user
+interface IActivationRequest {
+  activation_token: string;
+  activation_code: string;
+}
+export const activateUser = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { activation_token, activation_code } =
+        req.body as IActivationRequest;
+      const newUser: { user: IUser; activationCode: string } = jwt.verify(
+        activation_token,
+        process.env.ACTIVATION_SECRET as string
+      ) as { user: IUser; activationCode: string };
+      if (newUser.activationCode !== activation_code) {
+        return next(new ErrorHandler("Invalid activation code", 400));
+      }
+      const { name, email, password } = newUser.user;
+      const existUser = await userModel.findOne({ email });
+      if (existUser) {
+        return next(new ErrorHandler("Email already exist", 400));
+      }
+      const user = await userModel.create({ name, email, password });
+      res.status(201).json({
+        success: true,
+        message: "Account activated successfully",
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  } );
+
+  //Login User
+  interface ILoginRequest {
+    email:string;
+    password: string;
+  }
+
+  export const loginUser = CatchAsyncError(async(req:Request, res:Response, next:NextFunction)=>{
+    try {
+        const {email, password} = req.body as ILoginRequest;
+        if(!email || !password){
+            return next(new ErrorHandler("Please enter email and password", 400));
+        }
+        const user = await userModel.findOne({email}).select("+password");
+        if(!user){
+          return next(new ErrorHandler("Invalid email or password", 400));
+        }
+        const isPasswordMatch = await user.comparePassword(password);
+        if(!isPasswordMatch){
+            return next(new ErrorHandler("Invalid email or password", 400));
+        }
+
+        sendToken(user, 200, res);
+    } catch (error:any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  })
+
+  //logout user
+  export const logoutUser = CatchAsyncError(async(req:Request, res:Response, next:NextFunction)=>{
+      try {
+        res.cookie("access_token", "", {maxAge:1});
+        res.cookie("refresh_token", "", {maxAge:1});
+        const userId = req.user?._id || '';
+        redis.del(userId);
+        res.status(200).json({
+          success: true,
+          message: "Logged out successfully", 
+        });
+      } catch (error:any) {
+          return next(new ErrorHandler(error.message, 400));
+      }
+  });
--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,7 +1,10 @@
 import express from 'express';
-import {registrationUser} from '../controllers/user.controller';
+import {activateUser, loginUser, logoutUser, registrationUser} from '../controllers/user.controller';
+import { isAuthenticated } from '../middlewares/auth';
 
 const userRouter = express.Router();
 userRouter.post('/registration', registrationUser);
-
+userRouter.post('/activate-user', activateUser);
+userRouter.post('/login', loginUser);
+userRouter.get('/logout', isAuthenticated, logoutUser);
 export default userRouter;
